cf6f76e9ec2f7c9636dbbc52ad6c767e0fb00213
Nahum Casco <40469753+nahumcasco@users.noreply.github.com>
2022-02-02 19:15:11 -0600

    Clase 1 febrero
    Vimos funciones de Fecha

---
+++ Programacion3IPAC2022/Programacion3IPAC2022/FuncionesFecha.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Programacion3IPAC2022
+{
+    partial class FuncionesFecha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.ProcesarButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DiatextBox = new System.Windows.Forms.TextBox();
+            this.MestextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AniotextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DiaLetrasTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MesLetrastextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SumaDiastextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.EdadTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DateTimePicker
+            // 
+            this.DateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DateTimePicker.Location = new System.Drawing.Point(220, 12);
+            this.DateTimePicker.Name = "DateTimePicker";
+            this.DateTimePicker.Size = new System.Drawing.Size(147, 23);
+            this.DateTimePicker.TabIndex = 0;
+            // 
+            // ProcesarButton
+            // 
+            this.ProcesarButton.Location = new System.Drawing.Point(408, 12);
+            this.ProcesarButton.Name = "ProcesarButton";
+            this.ProcesarButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcesarButton.TabIndex = 1;
+            this.ProcesarButton.Text = "Procesar";
+            this.ProcesarButton.UseVisualStyleBackColor = true;
+            this.ProcesarButton.Click += new System.EventHandler(this.ProcesarButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dia";
+            // 
+            // DiatextBox
+            // 
+            this.DiatextBox.Location = new System.Drawing.Point(176, 77);
+            this.DiatextBox.Name = "DiatextBox";
+            this.DiatextBox.Size = new System.Drawing.Size(124, 23);
+            this.DiatextBox.TabIndex = 3;
+            // 
+            // MestextBox
+            // 
+            this.MestextBox.Location = new System.Drawing.Point(176, 106);
+            this.MestextBox.Name = "MestextBox";
+            this.MestextBox.Size = new System.Drawing.Size(124, 23);
+            this.MestextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mes";
+            // 
+            // AniotextBox
+            // 
+            this.AniotextBox.Location = new System.Drawing.Point(176, 135);
+            this.AniotextBox.Name = "AniotextBox";
+            this.AniotextBox.Size = new System.Drawing.Size(124, 23);
+            this.AniotextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Año";
+            // 
+            // DiaLetrasTextBox
+            // 
+            this.DiaLetrasTextBox.Location = new System.Drawing.Point(176, 177);
+            this.DiaLetrasTextBox.Name = "DiaLetrasTextBox";
+            this.DiaLetrasTextBox.Size = new System.Drawing.Size(124, 23);
+            this.DiaLetrasTextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Día en Letras";
+            // 
+            // MesLetrastextBox
+            // 
+            this.MesLetrastextBox.Location = new System.Drawing.Point(176, 206);
+            this.MesLetrastextBox.Name = "MesLetrastextBox";
+            this.MesLetrastextBox.Size = new System.Drawing.Size(124, 23);
+            this.MesLetrastextBox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Mes en Letras";
+            // 
+            // SumaDiastextBox
+            // 
+            this.SumaDiastextBox.Location = new System.Drawing.Point(176, 248);
+            this.SumaDiastextBox.Name = "SumaDiastextBox";
+            this.SumaDiastextBox.Size = new System.Drawing.Size(124, 23);
+            this.SumaDiastextBox.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sumar Días";
+            // 
+            // EdadTextBox
+            // 
+            this.EdadTextBox.Location = new System.Drawing.Point(176, 302);
+            this.EdadTextBox.Name = "EdadTextBox";
+            this.EdadTextBox.Size = new System.Drawing.Size(124, 23);
+            this.EdadTextBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Edad";
+            // 
+            // FuncionesFecha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 449);
+            this.Controls.Add(this.EdadTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.SumaDiastextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.MesLetrastextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DiaLetrasTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AniotextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MestextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DiatextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcesarButton);
+            this.Controls.Add(this.DateTimePicker);
+            this.Name = "FuncionesFecha";
+            this.Text = "FuncionesFecha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DateTimePicker DateTimePicker;
+        private Button ProcesarButton;
+        private Label label1;
+        private TextBox DiatextBox;
+        private TextBox MestextBox;
+        private Label label2;
+        private TextBox AniotextBox;
+        private Label label3;
+        private TextBox DiaLetrasTextBox;
+        private Label label4;
+        private TextBox MesLetrastextBox;
+        private Label label5;
+        private TextBox SumaDiastextBox;
+        private Label label6;
+        private TextBox EdadTextBox;
+        private Label label7;
+    }
+}
---
+++ Programacion3IPAC2022/Programacion3IPAC2022/FuncionesFecha.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion3IPAC2022
+{
+    public partial class FuncionesFecha : Form
+    {
+        public FuncionesFecha()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcesarButton_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = DateTimePicker.Value;
+
+            DiatextBox.Text = fecha.Day.ToString();
+            MestextBox.Text = fecha.Month.ToString();
+            AniotextBox.Text = fecha.Year.ToString();
+            
+            DiaLetrasTextBox.Text = fecha.ToString("dddd");
+            MesLetrastextBox.Text = fecha.ToString("MMMM");
+
+            SumaDiastextBox.Text = fecha.AddDays(25).ToShortDateString();
+
+            EdadTextBox.Text = DevolverEdad(fecha).ToString();
+        }
+
+        private int DevolverEdad(DateTime _fechaNacimiento)
+        {
+            DateTime fechaNacimiento = _fechaNacimiento;
+            DateTime fechaActual = DateTime.Now;
+
+            int edad = 0;
+
+            if (fechaNacimiento > fechaActual)
+            {
+                MessageBox.Show("La fecha de nacimiento es mayor a la fecha actual");
+                return 0;
+            }
+            {
+                edad = fechaActual.Year - fechaNacimiento.Year;
+                if (fechaNacimiento.Month > fechaActual.Month)
+                {
+                    --edad;
+                }
+            }
+
+            return edad;
+        }
+
+
+    }
+}
