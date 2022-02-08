a0e765ad616cfbfa7289533b082fa770942daa31
Nahum Casco <40469753+nahumcasco@users.noreply.github.com>
2022-02-08 14:53:08 -0600

    Clase 8 febrero
    Vimos programación Asíncrona

---
+++ Programacion3IPAC2022/Programacion3IPAC2022/FuncionesAsincronas.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Programacion3IPAC2022
+{
+    partial class FuncionesAsincronas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.HornearButton = new System.Windows.Forms.Button();
+            this.OtrosProcesosButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(108, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // HornearButton
+            // 
+            this.HornearButton.Location = new System.Drawing.Point(108, 82);
+            this.HornearButton.Name = "HornearButton";
+            this.HornearButton.Size = new System.Drawing.Size(141, 43);
+            this.HornearButton.TabIndex = 1;
+            this.HornearButton.Text = "Hornear Pizza";
+            this.HornearButton.UseVisualStyleBackColor = true;
+            this.HornearButton.Click += new System.EventHandler(this.HornearButton_Click);
+            // 
+            // OtrosProcesosButton
+            // 
+            this.OtrosProcesosButton.Location = new System.Drawing.Point(324, 82);
+            this.OtrosProcesosButton.Name = "OtrosProcesosButton";
+            this.OtrosProcesosButton.Size = new System.Drawing.Size(156, 43);
+            this.OtrosProcesosButton.TabIndex = 2;
+            this.OtrosProcesosButton.Text = "Otras tareas";
+            this.OtrosProcesosButton.UseVisualStyleBackColor = true;
+            this.OtrosProcesosButton.Click += new System.EventHandler(this.OtrosProcesosButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 282);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 326);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sumar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FuncionesAsincronas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 506);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OtrosProcesosButton);
+            this.Controls.Add(this.HornearButton);
+            this.Controls.Add(this.label1);
+            this.Name = "FuncionesAsincronas";
+            this.Text = "FuncionesAsincronas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button HornearButton;
+        private Button OtrosProcesosButton;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
